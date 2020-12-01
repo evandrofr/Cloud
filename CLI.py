4fd52911e18b76c083e6f8ea5544c3ec53b380b4
Evandro Romeiro <evandrofr@al.insper.edu.br>
2020-11-30 22:07:49 -0300

    Client

---
+++ CLI.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from sys import argv
+from datetime import datetime
+
+url = "http://lborm-642570112.us-west-2.elb.amazonaws.com/tasks/"  # url pega manualmente
+
+COMMANDS = ["get", "add", "delete"]
+
+if __name__ == "__main__":
+    if argv[1] == COMMANDS[0]:
+        print("Procurando rota /get_all...")
+        response = requests.get(url + "get_all")
+        print(response.text)
+    elif argv[1] == COMMANDS[1]:
+        if len(argv) == 4:
+            print("Procurando rota /add_task...")
+            title = argv[2]
+            desc  = argv[3]
+            date = datetime.now().isoformat()
+            data = json.dumps({'title':title, 'pub_date':date, 'description': desc})
+            response = requests.post(url + "add_task", data=data)
+            print(response.text)
+        else:
+            print("Numero de argumentos incorreto.")
+    elif argv[1] == COMMANDS[2]:
+        print("Procurando rota /clear...")
+        response = requests.delete(url + "clear")
+        print(response.text)
+    else:
+        print("Comando invalido")
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Cloud
+
+## COMO UTILIZAR O CLI.py:
+### CLI possui 3 comandos get, add e delete.
+- Get: pega todas as task do banco de dados. Retorna em formato json.
+- Add: adiciona nova task no banco de dados. Recebe um string para Title e outra para description. Retorna a task adiciona em formado json.
+- Delete: deleta todas as tasks do banco de dados.
+### COMANDOS
+```
+python CLI.py get
+python CLI.py add title description
+python CLI.py delete
+```
+
+
